f4517c02c4a421bc64ca894b23a87aef420be70e
HLonlin <1021944439@qq.com>
2020-11-17 18:52:45 +0800

    修改展示规则

--- src/common/axios/request.js
+++ src/common/axios/request.js
@@ -37,9 +37,9 @@ axios.interceptors.response.use(
     // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
     // 否则的话抛出错误
     if (response.status === 200) {
-      return Promise.resolve(response);
+      return Promise.resolve(response.data);
     } else {
-      return Promise.reject(response);
+      return Promise.reject(response.data);
     }
   },
   // 服务器状态码不是2开头的的情况
--- src/components/index.vue
+++ src/components/index.vue
@@ -3,8 +3,8 @@
     <div class="slider">
       <!-- slides -->
       <div class="slider__slides">
-        <div class="slide" :class="{ 's--active': i==0, 's--prev': i==adlist.length-1 }" v-for="(item,i) in adlist"
-          :key="i">
+        <div v-for="(item,i) in adlist" class="slide" :class="{ 's--active': i==0 }" :key="i" :data-slide="i+1"
+          :data-type='item.linkType'>
           <div v-if="item.linkType==='image'" class="slide__inner" :style="{backgroundImage: 'url(' + item.imgUrl + ')' }">
             <div class="slide__content__box">
               <!-- <div class="slide__content">
@@ -15,8 +15,7 @@
           </div>
           <div v-else-if="item.linkType==='video'" class="slide__inner">
             <div class="slide__content__box">
-              <video class="slide__inner--video" :src="item.url" :style="{backgroundImage: 'url(' + item.imgUrl + ')' }">
-                <!-- <video class="slide__inner--video" :src="item.url" :poster="item.imgUrl"> -->
+              <video muted controls class="slide__inner--video" :src="item.url">
                 您的浏览器不支持播放此视频。
               </video>
             </div>
@@ -24,14 +23,14 @@
         </div>
       </div>
       <!-- slides end -->
-      <div class="slider__control slider__control--left" @click="controlClickHandler($event,false)">
+      <!-- <div class="slider__control slider__control--left" @click="controlClickHandler($event,false)">
         <div class="slider__control-line"></div>
         <div class="slider__control-line"></div>
       </div>
       <div class="slider__control slider__control--right" @click="controlClickHandler($event,true)">
         <div class="slider__control-line"></div>
         <div class="slider__control-line"></div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -41,10 +40,10 @@
     name: "index",
     data() {
       return {
-        numOfSlides: 0,
-        slidingBlocked: false,
-        slidingAT: 600,
         adlist: [],
+        slidingBlocked: false,
+        numOfSlides: 0,
+        autoPlayTime: 800,
       };
     },
     beforeCreate() {},
@@ -63,59 +62,62 @@
       getAdlist() { // 获取广告列表
         let that = this;
         that.$axios.get(that.$api.adList, {
-          device: '广告屏'
-        }).then(res => {
-          let testArr = [];
-          for (let i = 0, imax = 6; i < imax; i++) {
-            let testItem = {
-              "indexNum": i,
-              "endDate": "2020-11-06 18:57:21",
-              "createTimeStamp": "",
-              "creatorId": i,
-              "creatorName": "系统管理员",
-              "url": "@/assets/video/TXbgAgfaokkA.mp4",
-              "imgUrl": "@/assets/img/" + (i + 1) + ".jpg",
-              "createTime": "16:40:55",
-              "name": "广告",
-              "linkType": "image",
-              "id": i,
-              "device": "设备",
-              "startDate": "2020-11-06 18:57:20",
-              "createDate": "2020-11-06",
-              "status": 0
-            };
-            switch (i) {
-              case 0:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/0199ff5fb23adc11013fdcc76ab3ff.jpg@3000w_1l_0o_100sh.jpg";
-                break;
-              case 1:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/01adc55fb2372711013fdcc7eb1456.jpg@1280w_1l_2o_100sh.jpg";
-                break;
-              case 2:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/01def95fb2373611013ee04d46772d.jpg@1280w_1l_2o_100sh.jpg";
-                break;
-              case 3:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/0157bd5fb21d8111013ee04db4cd84.jpg@1280w_1l_2o_100sh.jpg";
-                break;
-              case 4:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/0141ba5fb21d8111013fdcc799a838.jpg@1280w_1l_2o_100sh.jpg";
-                break;
-              case 5:
-                testItem.imgUrl =
-                  "https://img.zcool.cn/community/01bfc15fb21ca211013ee04d17bf5d.jpg@1280w_1l_2o_100sh.jpg";
-                testItem.linkType = 'video';
-                testItem.url =
-                  "https://1252326633.vod2.myqcloud.com/4d86cec3vodcq1252326633/1d04542e5285890809824326316/TXbgAgfaokkA.mp4"
-                break;
-            }
-            testArr.push(testItem);
-            res.data = testArr;
+          params: {
+            'device': '广告屏'
           }
+        }).then(res => {
+          // let testArr = [];
+          // for (let i = 0, imax = 6; i < imax; i++) {
+          //   let testItem = {
+          //     "indexNum": i,
+          //     "endDate": "2020-11-06 18:57:21",
+          //     "createTimeStamp": "",
+          //     "creatorId": i,
+          //     "creatorName": "系统管理员",
+          //     "url": "@/assets/video/TXbgAgfaokkA.mp4",
+          //     "imgUrl": "@/assets/img/" + (i + 1) + ".jpg",
+          //     "createTime": "16:40:55",
+          //     "name": "广告",
+          //     "linkType": "image",
+          //     "id": i,
+          //     "device": "设备",
+          //     "startDate": "2020-11-06 18:57:20",
+          //     "createDate": "2020-11-06",
+          //     "status": 0,
+          //     "autoPlayTime": 800
+          //   };
+          //   switch (i) {
+          //     case 0:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/0199ff5fb23adc11013fdcc76ab3ff.jpg@3000w_1l_0o_100sh.jpg";
+          //       break;
+          //     case 1:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/01adc55fb2372711013fdcc7eb1456.jpg@1280w_1l_2o_100sh.jpg";
+          //       break;
+          //     case 2:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/01def95fb2373611013ee04d46772d.jpg@1280w_1l_2o_100sh.jpg";
+          //       break;
+          //     case 3:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/0157bd5fb21d8111013ee04db4cd84.jpg@1280w_1l_2o_100sh.jpg";
+          //       break;
+          //     case 4:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/0141ba5fb21d8111013fdcc799a838.jpg@1280w_1l_2o_100sh.jpg";
+          //       break;
+          //     case 5:
+          //       testItem.imgUrl =
+          //         "https://img.zcool.cn/community/01bfc15fb21ca211013ee04d17bf5d.jpg@1280w_1l_2o_100sh.jpg";
+          //       testItem.linkType = 'video';
+          //       testItem.url =
+          //         "https://1252326633.vod2.myqcloud.com/4d86cec3vodcq1252326633/1d04542e5285890809824326316/TXbgAgfaokkA.mp4"
+          //       break;
+          //   }
+          //   testArr.push(testItem);
+          //   res.data = testArr;
+          // }
 
           console.log('Handle the success event:', res.data);
           for (let i = 0, imax = res.data.length; i < imax; i++) {
@@ -134,32 +136,65 @@
           for (let i = 0, imax = slides.length; i < imax; i++) {
             let index = i + 1;
             slides[i].classList.add('slide-' + index);
-            slides[i].dataset.slide = index;
           }
+          that.autoPlaySilder();
         });
       },
-      controlClickHandler(event, isRight) { //点击翻页
+      // controlClickHandler(event, isRight) { //点击翻页
+      //   let that = this;
+      //   if (that.slidingBlocked) return;
+      //   that.slidingBlocked = true;
+      //   let $control = event.target;
+      //   let $curActive = document.querySelector('.slide.s--active');
+      //   let index = $curActive.dataset.slide;
+
+      //   (isRight) ? index++ : index--;
+      //   if (index < 1) index = that.numOfSlides;
+      //   if (index > that.numOfSlides) index = 1;
+      //   let $newActive = document.querySelector('.slide-' + index);
+
+      //   $control.classList.add('a--rotation');
+      //   $curActive.classList.remove('s--active');
+      //   $newActive.classList.add('s--active');
+
+      //   setTimeout(function() {
+      //     $control.classList.remove('a--rotation');
+      //     that.slidingBlocked = false;
+      //   }, that.slidingAT);
+      // },
+      autoPlaySilder() { //自动轮播
         let that = this;
-        if (that.slidingBlocked) return;
+        if (that.slidingBlocked || that.adlist.length <= 0) return;
         that.slidingBlocked = true;
-        let $control = event.target;
         let $curActive = document.querySelector('.slide.s--active');
         let index = $curActive.dataset.slide;
-
-        (isRight) ? index++ : index--;
-        if (index < 1) index = that.numOfSlides;
-        if (index > that.numOfSlides) index = 1;
-        let $newActive = document.querySelector('.slide-' + index);
-
-        $control.classList.add('a--rotation');
-        $curActive.classList.remove('s--active');
-        $newActive.classList.add('s--active');
-
-        setTimeout(function() {
-          $control.classList.remove('a--rotation');
-          that.slidingBlocked = false;
-        }, that.slidingAT);
-      },
+        let $curAd = that.adlist[index - 1];
+        if ($curAd.linkType === "image") {
+          that.autoPlayTime = $curAd.autoPlayTime;
+          let timer_out = setTimeout(function() {
+            index++;
+            if (index > that.numOfSlides) index = 1;
+            let $newActive = document.querySelector('.slide-' + index);
+            $curActive.classList.remove('s--active');
+            $newActive.classList.add('s--active');
+            that.slidingBlocked = false;
+            clearTimeout(timer_out);
+            that.autoPlaySilder();
+          }, that.autoPlayTime);
+        } else if ($curAd.linkType === "video") {
+          let videoer = $curActive.children[0].children[0].children[0];
+          videoer.play();
+          videoer.addEventListener('ended', function() { //结束
+            index++;
+            if (index > that.numOfSlides) index = 1;
+            let $newActive = document.querySelector('.slide-' + index);
+            $curActive.classList.remove('s--active');
+            $newActive.classList.add('s--active');
+            that.slidingBlocked = false;
+            that.autoPlaySilder();
+          }, false);
+        }
+      }
     },
   };
 </script>
